063029a7ba262b2071e2226bf25669175298038a
Mikolaj Rybinski <mikolajrybinski@outlook.com>
2018-01-17 18:37:29 +0100

    Added routing in spa

---
+++ DatingApp.SPA/src/app/_guards/auth.guard.ts
@@ -0,0 +1,24 @@
+import { Injectable } from '@angular/core';
+import { CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot } from '@angular/router';
+import { Observable } from 'rxjs/Observable';
+import { AuthService } from '../_services/auth.service';
+import { Router } from '@angular/router';
+import { AlertifyService } from '../_services/alertify.service';
+
+@Injectable()
+export class AuthGuard implements CanActivate {
+
+  constructor( private authService: AuthService, private router: Router, private alertify: AlertifyService){
+
+  }
+
+  canActivate(): Observable<boolean> | Promise<boolean> | boolean {
+    if ( this.authService.loggedIn()) {
+      return true;
+    }
+
+    this.alertify.error('You need to be logged in to acces this area');
+    this.router.navigate(['/home']);
+    return false;
+  }
+}
